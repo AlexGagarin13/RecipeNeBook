9a1264b5ee4e4de551b47ed9b029a1e3b364c242
Aleksey Anokhin <anoxin-aleksey@bk.ru>
2022-05-31 16:40:01 +0300

    Установлена иконка Созданы наброски требуемых пакетов Начата верстка layout страницы рецепта

---
+++ app/src/main/java/ru/netology/recipenebook/adapter/RecipeAdapter.kt
@@ -0,0 +1,4 @@
+package ru.netology.recipenebook.adapter
+
+class RecipeAdapter {
+}
---
+++ app/src/main/java/ru/netology/recipenebook/ui/AppActivity.kt
@@ -0,0 +1,9 @@
+package ru.netology.recipenebook.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.recipenebook.R
+
+
+class AppActivity:AppCompatActivity(R.layout.app_activity) {
+
+}
